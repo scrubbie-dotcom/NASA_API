d3d279111a475633786834f2d1b5d2cbf2b1b658
scrubbie-dotcom <hnyehkrlopehele@gmail.com>
2024-10-05 12:13:48 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -61,4 +61,4 @@ def get_planet_by_name(planet_name: str):
 # If needed for local testing
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(fs.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
